c3f10f58f1e751aec8b49072326df03fcf5c1268
Fabrice CLAEYS <claeys.fabrice@gmail.com>
2022-07-20 23:09:10 +0200

    generate auth

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -1,8 +1,20 @@
 defmodule PentoWeb.WrongLive do
   use Phoenix.LiveView, layout: {PentoWeb.LayoutView, "live.html"}
 
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:", time: time(), number: :rand.uniform(10))}
+  alias Pento.Account
+
+  def mount(_params, session, socket) do
+    user = Account.get_user_by_session_token(session["user_token"])
+    {
+      :ok,
+      assign(
+        socket,
+        score: 0,
+        message: "Make a guess:",
+        time: time(),
+        number: :rand.uniform(10),
+        session_id: session["live_socket_id"],
+        current_user: user)}
   end
 
   def render(assigns) do
@@ -17,6 +29,10 @@ defmodule PentoWeb.WrongLive do
           <a href="#" phx-click="guess" phx-value-number= {n}><%= n %></a>
         <% end %>
       </h2>
+      <pre>
+        <%= @current_user.email %>
+        <%= @session_id %>
+      </pre>
     """
   end
 
