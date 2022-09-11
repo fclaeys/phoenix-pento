214355ad620f53b7a920fbf8bf45258f202392d9
Fabrice CLAEYS <claeys.fabrice@gmail.com>
2022-09-11 16:48:32 +0200

    generate catalog and product

---
+++ lib/pento/catalog/product.ex
@@ -0,0 +1,27 @@
+defmodule Pento.Catalog.Product do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "products" do
+    field :description, :string
+    field :name, :string
+    field :sku, :integer
+    field :unit_price, :float
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(product, attrs) do
+    product
+    |> cast(attrs, [:name, :description, :unit_price, :sku])
+    |> validate_required([:name, :description, :unit_price, :sku])
+    |> unique_constraint(:sku)
+  end
+
+  def markdown_product(product, attrs) do
+    product
+    |> cast(attrs, [:unit_price])
+    |> validate_number(:unit_price, less_than: product.unit_price)
+  end
+end
